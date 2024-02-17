1dd24008ce4926f62671006ebab3d868edeb637d
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-17 16:33:27 -0500

    add a first experience

--- app/src/pages/Experience.jsx
+++ app/src/pages/Experience.jsx
@@ -4,8 +4,9 @@ function ExperiencePage() {
   return (
     <>
       <main>
-        <section>
-          <h2>Experience page</h2>
+        <section className='home-wrapper'>
+          <h1 className='max-emphasis'>2022</h1>
+          <h2>Ubisoft</h2>
         </section>
       </main>
     </>
