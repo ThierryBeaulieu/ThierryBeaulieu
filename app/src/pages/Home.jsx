894d7d9be4d6f8ab4c78993a0fe151ad26e53dba
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-17 15:57:04 -0500

    add my picture beside a description that shows who I am

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -4,7 +4,40 @@ function HomePage() {
   return (
     <>
       <main>
-        <section>
+        <section className='home-wrapper'>
+          <div className='personal-presentation'>
+            <div className='description-container'>
+              <h1 className='max-emphasis'>Hello world!</h1>
+              <p className='description-content'>
+                <p>
+                  Hello! Allow me to introduce myself, I'm a software engineering student at
+                  Polytechnique Montreal.
+                </p>
+
+                <p>
+                  I have acquired valuable professional experience in both the gaming industry and
+                  cybersecurity. These roles have significantly contributed to my skill set,
+                  providing me with comprehensive knowledge and expertise in these dynamic fields.
+                </p>
+
+                <p>
+                  I genuinely love diving into new tech stuff and teaming up with people. I'm always
+                  up for getting my hands dirty in fresh projects. To me, software is all about
+                  being creative, solving real problems, and working together. Can't wait to see
+                  what's next!
+                </p>
+              </p>
+            </div>
+            <div className='headShot-container'>
+              <img
+                alt='thierry-picture'
+                id='headShot'
+                src='../assets/headshot2.jpg'
+                className='headShot'
+              ></img>
+            </div>
+          </div>
+
           <div className='presentation-wrapper '>
             <div className='presentation-container'>
               <img alt='won-prices' id='CQI-2024' src='../assets/cqi-2024.JPG'></img>
@@ -12,14 +45,6 @@ function HomePage() {
           </div>
 
           <h2>Home page</h2>
-          <div className='headShot-container'>
-            <img
-              alt='thierry-picture'
-              id='headShot'
-              src='../assets/headshot2.jpg'
-              className='headShot'
-            ></img>
-          </div>
         </section>
       </main>
     </>
