d3e0746e078e479da2aa122d159b507e2d879a9e
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-17 17:29:12 -0500

    add genetec an experience

---
+++ app/src/components/Post.jsx
@@ -0,0 +1,41 @@
+import '../App.css';
+
+function Post({ experience }) {
+  return (
+    <div className='job-presentation'>
+      <div className='description-container'>
+        <h1 className='max-emphasis'>{experience.year}</h1>
+        <h2 className='company-name'>{experience.company}</h2>
+        <p className='description-content'>
+          <ul className='job-description'>
+            <li>
+              {experience.description1}
+              
+            </li>
+            <li>
+              {experience.description2}
+              
+            </li>
+            <li>
+              {experience.description3}
+              
+            </li>
+          </ul>
+        </p>
+      </div>
+      <div className='headShot-wrapper'>
+        <div className='logo-container'>
+          <img
+            alt='compagny-picture'
+            id='headShot'
+            src={experience.logo}
+            className='company-logo'
+          ></img>
+        </div>
+        <span className='duration-description'>{experience.duration}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
--- app/src/pages/Experience.jsx
+++ app/src/pages/Experience.jsx
@@ -1,43 +1,36 @@
 import '../App.css';
+import Post from '../components/Post';
 
 function ExperiencePage() {
+  const ubisoft = {
+    year: `Summer 2022`,
+    company: 'Ubisoft',
+    description1: `Integrated debugging functionalities into the ImGui debugging tool utilized in Assassin's Creed development.`,
+    description2: `Addressed hotkey conflicts, enhancing productivity with a streamlined workflow for development teams.`,
+    description3: `Conducted in-depth analysis of thread synchronization anomalies within the order of input event handling, ensuring seamless performance.`,
+    duration: `May 2022 - Aug 2022 · 4 months internship`,
+    logo: `../assets/Ubisoft_logo.png`,
+  };
+
+  const genetec = {
+    year: `Winter 2023`,
+    company: 'Genetec',
+    description1: `Efficiently tackled multiple bugs and regressions on custom images generated through the Yocto Project, resulting in uninterrupted device production and a substantial improvement in firmware effectiveness.`,
+    description2: `Engineered a firmware update that enables cameras to seamlessly migrate from Windows to Linux operating systems, significantly extending product lifespan, reducing maintenance costs, and up to a 45% reduction in disk space usage.`,
+    description3: `Acquired valuable knowledge about firmware, software, and hardware components of IoT devices, specifically cameras.`,
+    duration: `Jan 2023 - Apr 2023 · 4 months internship`,
+    logo: `../assets/genetec_logo.png`,
+  };
+
+  const experiences = [ubisoft, genetec];
+
   return (
     <>
       <main>
         <section className='home-wrapper'>
-          <div className='job-presentation'>
-            <div className='description-container'>
-              <h1 className='max-emphasis'>2022</h1>
-              <h2 className='company-name'>Ubisoft</h2>
-              <p className='description-content'>
-                <ul className='job-description'>
-                  <li>
-                    Integrated debugging functionalities into the ImGui debugging tool utilized in
-                    Assassin's Creed development.
-                  </li>
-                  <li>
-                    Addressed hotkey conflicts, enhancing productivity with a streamlined workflow
-                    for development teams.
-                  </li>
-                  <li>
-                    Conducted in-depth analysis of thread synchronization anomalies within the order
-                    of input event handling, ensuring seamless performance.
-                  </li>
-                </ul>
-              </p>
-            </div>
-            <div className='headShot-wrapper'>
-              <div className='logo-container'>
-                <img
-                  alt='thierry-picture'
-                  id='headShot'
-                  src='../assets/Ubisoft_logo.png'
-                  className='company-logo'
-                ></img>
-              </div>
-              <span className='duration-description'>Internship May 2022 - Aug 2022 · 4 mos</span>
-            </div>
-          </div>
+          {experiences.map((experience) => (
+            <Post experience={experience} key={experience} />
+          ))}
         </section>
       </main>
     </>
