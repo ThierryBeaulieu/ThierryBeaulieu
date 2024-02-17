2fb14a74533e91f2a567e94890ba31414dc93868
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-17 17:44:57 -0500

    add an iframe to show linkedin post

--- app/src/components/Post.jsx
+++ app/src/components/Post.jsx
@@ -2,38 +2,39 @@ import '../App.css';
 
 function Post({ experience }) {
   return (
-    <div className='job-presentation'>
-      <div className='description-container'>
-        <h1 className='max-emphasis'>{experience.year}</h1>
-        <h2 className='company-name'>{experience.company}</h2>
-        <p className='description-content'>
-          <ul className='job-description'>
-            <li>
-              {experience.description1}
-              
-            </li>
-            <li>
-              {experience.description2}
-              
-            </li>
-            <li>
-              {experience.description3}
-              
-            </li>
-          </ul>
-        </p>
-      </div>
-      <div className='headShot-wrapper'>
-        <div className='logo-container'>
-          <img
-            alt='compagny-picture'
-            id='headShot'
-            src={experience.logo}
-            className='company-logo'
-          ></img>
+    <div className='job-wrapper'>
+      <div className='job-presentation'>
+        <div className='description-container'>
+          <h1 className='max-emphasis'>{experience.year}</h1>
+          <h2 className='company-name'>{experience.company}</h2>
+          <p className='description-content'>
+            <ul className='job-description'>
+              <li>{experience.description1}</li>
+              <li>{experience.description2}</li>
+              <li>{experience.description3}</li>
+            </ul>
+          </p>
         </div>
-        <span className='duration-description'>{experience.duration}</span>
+        <div className='headShot-wrapper'>
+          <div className='logo-container'>
+            <img
+              alt='compagny-picture'
+              id='headShot'
+              src={experience.logo}
+              className='company-logo'
+            ></img>
+          </div>
+          <span className='duration-description'>{experience.duration}</span>
+        </div>
       </div>
+      <iframe
+        src='https://www.linkedin.com/embed/feed/update/urn:li:share:7100995938185265152'
+        height='476'
+        width='504'
+        frameBorder='0'
+        allowfullscreen=''
+        title='Embedded post'
+      ></iframe>
     </div>
   );
 }
