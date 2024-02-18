3ff5eb3735145dd9fe28c1d81f761d6d731b0be1
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-17 20:41:48 -0500

    add fake implementation of element for the page

--- app/src/pages/Projects.jsx
+++ app/src/pages/Projects.jsx
@@ -1,11 +1,28 @@
 import '../App.css';
+import ProjectPost from '../components/ProjectPost';
 
 function ProjectPage() {
+  const ubisoft = {
+    year: `Summer 2022`,
+    title: `Game Engine Developer Intern`,
+    company: 'Ubisoft',
+    description1: `Integrated debugging functionalities into the ImGui debugging tool utilized in Assassin's Creed development.`,
+    description2: `Addressed hotkey conflicts, enhancing productivity with a streamlined workflow for development teams.`,
+    description3: `Conducted in-depth analysis of thread synchronization anomalies within the order of input event handling, ensuring seamless performance.`,
+    duration: `May 2022 - Aug 2022 · 4 months internship`,
+    logo: `../assets/Ubisoft_logo.png`,
+    linkedInSrc: `https://www.linkedin.com/embed/feed/update/urn:li:share:6937981961403609088`,
+  };
+
+  const project = [ubisoft];
+
   return (
     <>
       <main>
         <section>
-          <h2>Project page</h2>
+          {project.map((project) => (
+            <ProjectPost project={project} key={project} />
+          ))}
         </section>
       </main>
     </>
