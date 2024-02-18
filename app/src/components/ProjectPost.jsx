62b256ee3a771db8d5433ec6225df726cdad3cb6
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-17 20:37:41 -0500

    create a new component that will be resusable

---
+++ app/src/components/ProjectPost.jsx
@@ -0,0 +1,44 @@
+import '../App.css';
+
+function ProjectPost({ project }) {
+  return (
+    <div className='job-wrapper'>
+      <div className='job-presentation'>
+        <div className='description-container'>
+          <h1 className='max-emphasis'>{project.year}</h1>
+          <h2 className='company-name'>{project.company}</h2>
+          <p className='description-content'>
+            <ul className='job-description'>
+              <li>{project.description1}</li>
+              <li>{project.description2}</li>
+              <li>{project.description3}</li>
+            </ul>
+          </p>
+        </div>
+        <div className='headShot-wrapper'>
+          <div className='logo-container'>
+            <img
+              alt='compagny-picture'
+              id='headShot'
+              src={project.logo}
+              className='company-logo'
+            ></img>
+          </div>
+          <span className='duration-description'>{project.duration}</span>
+        </div>
+      </div>
+      {project.linkedInSrc && (
+        <iframe
+          src={project.linkedInSrc}
+          height='500'
+          width='600'
+          frameBorder='0'
+          allowfullscreen=''
+          title='Embedded post'
+        ></iframe>
+      )}
+    </div>
+  );
+}
+
+export default ProjectPost;
