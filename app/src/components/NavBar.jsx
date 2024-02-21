385e6ed27054c395aa7754915dd8384e433d7534
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-02-21 15:16:09 -0500

    React seems to work with bootstrap and reactboostrap

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -1,10 +1,35 @@
 import '../App.css';
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <>
-      <header>
+    <Navbar expand='lg'>
+      <Navbar.Brand as={Link} to='/'>
+        Thierry Beaulieu
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link as={Link} to='/experiences'>
+            Expériences
+          </Nav.Link>
+          <Nav.Link as={Link} to='/projects'>
+            Projets
+          </Nav.Link>
+          <Nav.Link as={Link} to='/about'>
+            About
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavBar;
+
+/*
         <nav>
           <ul>
             <li>
@@ -24,9 +49,4 @@ function NavBar() {
             </li>
           </ul>
         </nav>
-      </header>
-    </>
-  );
-}
-
-export default NavBar;
+*/
