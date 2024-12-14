c9dd10b954c27903a759c14e8aa71a33ab6e4c10
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-12-14 12:34:08 -0500

    remove footer to use bootstrap instead

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -4,17 +4,6 @@ function Footer() {
   return (
     <>
       <footer>
-        <div className='footer-wrapper'>
-          <a href='https://www.linkedin.com/in/thierrybeaulieu/'>
-            <i className='fa-brands fa-linkedin'></i> LinkedIn
-          </a>
-          <a href='https://github.com/ThierryBeaulieu'>
-            <i className='fa-brands fa-github'></i> Github
-          </a>
-          <a href='mailto:thierry.abeaulieu@gmail.com'>
-            <i className='fa-regular fa-envelope'></i> thierry.abeaulieu@gmail.com
-          </a>
-        </div>
       </footer>
     </>
   );
