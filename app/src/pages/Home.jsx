c5c60cb27e993feaa76548163feff251d96afe21
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-12-15 00:12:49 -0500

    add adaptive layout to display image of the cqi 2024 to the right and under the text when wrappign the image

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -5,18 +5,7 @@ function HomePage() {
     <>
       <main>
         <div className='container col-xxl-8 px-4 py-5'>
-          <div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
-            <div className='col-10 col-sm-8 col-lg-6'>
-              <img
-                src='../assets/cqi-2024.JPG'
-                className='d-block mx-lg-auto img-fluid'
-                alt='Bootstrap Themes'
-                width='700'
-                height='500'
-                loading='lazy'
-              />
-            </div>
-
+          <div className='row flex-lg-row align-items-center g-5 py-5'>
             <div className='col-lg-6'>
               <h1 className='display-5 fw-bold text-body-emphasis lh-1 mb-3'>
                 Responsive left-aligned hero with image
@@ -35,6 +24,17 @@ function HomePage() {
                   Default
                 </button>
               </div>
+            </div>
+
+            <div className='col-12 col-lg-6'>
+              <img
+                src='../assets/cqi-2024.JPG'
+                className='img-fluid border rounded-3 shadow-lg mb-4'
+                alt='winnner of the CQI 2024'
+                width='700'
+                height='500'
+                loading='lazy'
+              />
             </div>
           </div>
         </div>
