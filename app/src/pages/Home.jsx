3f1b33148671cc339814fdee5f42ac1c3d0e0e30
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-12-15 00:21:56 -0500

    remove buttons

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -16,14 +16,6 @@ function HomePage() {
                 mixins, responsive grid system, extensive prebuilt components, and powerful
                 JavaScript plugins.
               </p>
-              <div className='d-grid gap-2 d-md-flex justify-content-md-start'>
-                <button type='button' className='btn btn-primary btn-lg px-4 me-md-2'>
-                  Primary
-                </button>
-                <button type='button' className='btn btn-outline-secondary btn-lg px-4'>
-                  Default
-                </button>
-              </div>
             </div>
 
             <div className='col-12 col-lg-6'>
