c2d5457798a1c93eede8e1cec6635a811ce23a02
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-12-15 01:10:25 -0500

    save dir

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -24,15 +24,36 @@ function HomePage() {
                   alt='winner of the CQI 2024'
                   loading='lazy'
                 />
-                <figcaption className='mt-2'>
-                  <p>
-                    <em>
-                      Winners of the 2024 edition of the Quebec Engineering Competition for the
-                      programming category (CEC)
-                    </em>
-                  </p>
-                </figcaption>
               </figure>
+            </div>
+          </div>
+        </div>
+
+        <div className='px-4 pt-5 text-center border-bottom'>
+          <div className='overflow-hidden'>
+            <div className='container px-5'>
+              <img
+                src='../assets/cqi-2024.JPG'
+                className='img-fluid border rounded-3 shadow-lg mb-2'
+                alt='CQI winners'
+                width='700'
+                height='500'
+                loading='lazy'
+              />
+              <figcaption className='mt-2'>
+                <p>
+                  <em>
+                    Winners of the 2024 edition of the Quebec Engineering Competition for the
+                    programming category (CEC)
+                  </em>
+                </p>
+                <p>
+                  <small>
+                    From left to right: Juge #1, Juge #2, Dominic Hains, Maxime Gaudreau, Gabriel
+                    Côté, Thierry Beaulieu (me), Juge #3
+                  </small>
+                </p>
+              </figcaption>
             </div>
           </div>
         </div>
